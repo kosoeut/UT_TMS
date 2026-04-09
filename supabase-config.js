f5fff6aaecba1059ca8633d76f6ba46f1203fe1d
kosoeut <kosoe.ut@gmail.com>
2026-04-10 00:47:11 +0630

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://yjoszqcnfhqucdjfnryn.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_E_ZX9d1sY7ayRieKMFWPGw_sO07v050'; 
+
+window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
